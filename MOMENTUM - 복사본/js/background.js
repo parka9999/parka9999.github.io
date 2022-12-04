a29a3974ad8ca419e083fee65eea101d00e192c3
parka9999 <109406750+parka9999@users.noreply.github.com>
2022-12-04 18:34:02 +0900

    Delete MOMENTUM - 복사본 directory

--- MOMENTUM - 복사본/js/background.js
+++
@@ -1,8 +0,0 @@
-const image = ["0.jpeg", "1.jpeg", "2.jpeg"]
-
-const chosenImage = image[Math.floor(Math.random() * image.length)];
-
-const bodyStyle = document.body.style;
-bodyStyle.background = `url('img/${chosenImage}') no-repeat`;
-bodyStyle.backgroundSize = 'cover';
-
